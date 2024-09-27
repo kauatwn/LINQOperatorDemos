85896696c6cfbfbcf8a3ae5ed3f91a98b03b1b1f
kauatwn <kaualopescontato@gmail.com>
2024-09-27 20:12:27 -0300

    Filtering

---
+++ src/Filtering/Entities/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Filtering.Entities;
+
+internal class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Price { get; set; }
+}
---
+++ src/Filtering/Program.cs
@@ -0,0 +1,29 @@
+﻿using Filtering.Entities;
+
+var items = new List<object>
+{
+    new Product { Id = 1, Name = "Apple", Price = 1.20 },
+    new Product { Id = 2, Name = "Banana", Price = 2.40 },
+    new Product { Id = 3, Name = "Carrot", Price = 0.80 },
+    new Product { Id = 4, Name = "Lettuce", Price = 1.00 },
+    new Category { Id = 1, Name = "Fruit" },
+    new Category { Id = 2, Name = "Vegetable" }
+};
+
+// Filtering
+var products = items.OfType<Product>();
+Console.WriteLine("Lista de produtos:");
+
+foreach (var product in products)
+{
+    Console.WriteLine($"Id: {product.Id}, Nome: {product.Name}, Preço: {product.Price:C}");
+}
+
+// Filtering with condition
+var expensiveProducts = items.OfType<Product>().Where(p => p.Price > 1.00);
+Console.WriteLine("\nProdutos com preço maior que R$ 1,00:");
+
+foreach (var product in expensiveProducts)
+{
+    Console.WriteLine($"Id: {product.Id}, Nome: {product.Name}, Preço: {product.Price:C}");
+}
