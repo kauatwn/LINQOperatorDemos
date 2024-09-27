389b4e919f4a601db28fe92c45b03d7b478d2d70
kauatwn <kaualopescontato@gmail.com>
2024-09-27 19:18:34 -0300

    Projection

---
+++ src/Projection/Entities/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Projection.Entities;
+
+internal class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Price { get; set; }
+}
---
+++ src/Projection/Program.cs
@@ -0,0 +1,26 @@
+﻿using Projection.Entities;
+
+var products = new List<Product>
+{
+    new() { Id = 1, Name = "Apple", Price = 1.20 },
+    new() { Id = 2, Name = "Banana", Price = 2.40 },
+    new() { Id = 3, Name = "Cherry", Price = 3.60 }
+};
+
+// Simple projection
+var productNames = products.Select(p => p.Name);
+Console.WriteLine("Projeção simples dos produtos:");
+
+foreach (var name in productNames)
+{
+    Console.WriteLine($"Nome: {name}");
+}
+
+// Complex projection
+var productProjection = products.Select(p => new { p.Id, p.Name, p.Price });
+Console.WriteLine("\nProjeção complexa dos produtos:");
+
+foreach (var product in productProjection)
+{
+    Console.WriteLine($"Id: {product.Id}, Nome: {product.Name}, Preço: {product.Price:C}");
+}
