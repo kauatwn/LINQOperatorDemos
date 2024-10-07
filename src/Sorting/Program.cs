759be3f85e7ec611ed89abf2195f9e9e20a200d4
kauatwn <kaualopescontato@gmail.com>
2024-10-07 14:35:42 -0300

    Sorting

--- src/Sorting/Program.cs
+++ src/Sorting/Program.cs
@@ -9,7 +9,8 @@ var people = new List<Person>
     new("Bob", "Williams", 22)
 };
 
-// 1. Sort by name in ascending order
+// 1. OrderBy
+// It orders by name in ascending order.
 var sortedByName = people.OrderBy(p => p.FirstName);
 Console.WriteLine("Pessoas ordenadas pelo nome em ordem crescente:");
 
@@ -18,7 +19,8 @@ foreach (var person in sortedByName)
     Console.WriteLine($"{person.FirstName} {person.LastName}, {person.Age} anos");
 }
 
-// 2. Sort by name in descending order
+// 2. OrderByDescending
+// It orders by name in descending order.
 var sortedByNameDesc = people.OrderByDescending(p => p.FirstName);
 Console.WriteLine("\nPessoas ordenadas pelo nome em ordem decrescente:");
 
@@ -27,7 +29,8 @@ foreach (var person in sortedByNameDesc)
     Console.WriteLine($"{person.FirstName} {person.LastName}, {person.Age} anos");
 }
 
-// 3. Sort by age and then by name in ascending order
+// 3. ThenBy
+// It orders by name in ascending order after ordering by age in ascending order.
 var sortedByAgeAndName = people.OrderBy(p => p.Age).ThenBy(p => p.FirstName);
 Console.WriteLine("\nPessoas ordenadas pela idade e depois pelo nome em ordem crescente:");
 
@@ -36,7 +39,8 @@ foreach (var person in sortedByAgeAndName)
     Console.WriteLine($"{person.FirstName} {person.LastName}, {person.Age} anos");
 }
 
-// 4. Sort by age and then by name in descending order
+// 4. ThenByDescending
+// It orders by name in descending order after ordering by age in descending order.
 var sortedByAgeAndNameDesc = people.OrderByDescending(p => p.Age).ThenByDescending(p => p.FirstName);
 Console.WriteLine("\nPessoas ordenadas pela idade e depois pelo nome em ordem decrescente:");
 
@@ -45,7 +49,9 @@ foreach (var person in sortedByAgeAndNameDesc)
     Console.WriteLine($"{person.FirstName} {person.LastName}, {person.Age} anos");
 }
 
-// 5. Reverse the order of list after sorting by age in ascending order (equivalent to sort by age in descending order directly)
+// 5. Reverse
+// It reverses the list after ordering by age in ascending order (equivalent to sort by age in descending order
+// directly)
 var reversedList = people.OrderBy(p => p.Age).Reverse();
 
 Console.WriteLine("\nReverte a ordem da lista após ordenar pela idade:");
@@ -53,3 +59,6 @@ foreach (var person in reversedList)
 {
     Console.WriteLine($"{person.FirstName} {person.LastName}, {person.Age} anos");
 }
+
+// It is not recommended to use the Reverse method after the OrderBy or OrderByDescending methods. It can be
+// confusing and make the code less readable. It is better to use the OrderBy or OrderByDescending methods directly.
