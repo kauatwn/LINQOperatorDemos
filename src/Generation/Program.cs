cab8d358e1800c356c801993daf2aba7beb6244c
kauatwn <kaualopescontato@gmail.com>
2024-10-16 14:33:46 -0300

    Generation

---
+++ src/Generation/Program.cs
@@ -0,0 +1,25 @@
+﻿// 1. Empty
+var emptyNumbers = Enumerable.Empty<int>();
+
+if (!emptyNumbers.Any())
+{
+    Console.WriteLine("A sequência está vazia");
+}
+
+// 2. Range
+var rangeNumbers = Enumerable.Range(1, 10);
+
+foreach (var number in rangeNumbers)
+{
+    Console.Write(number + " ");
+}
+
+Console.WriteLine();
+
+// 3. Repeat
+var repeatNumbers = Enumerable.Repeat(2, 5);
+
+foreach (var number in repeatNumbers)
+{
+    Console.Write(number + " ");
+}
